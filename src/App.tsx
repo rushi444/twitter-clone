54fa1f44809b31b05f29a3d2494102ae17f68a97
rushi444 <rushi44@gmail.com>
2020-06-15 14:20:54 -0700

    move to material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,83 @@
 import React from 'react';
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
+import './App.css';
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
-import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import { authFormFields } from './utils/auth';
-import './App.css';
+import {
+  makeStyles,
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Amplify from '@aws-amplify/core';
+import PubSub from '@aws-amplify/pubsub';
+import awsconfig from './aws-exports';
 
 Amplify.configure(awsconfig);
+PubSub.configure(awsconfig);
+
+const drawerWidth = 240;
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#1EA1F2',
+      contrastText: '#fff',
+    },
+    background: {
+      default: '#15202B',
+      paper: '#15202B',
+    },
+    divider: '#37444C',
+  },
+  overrides: {
+    MuiButton: {
+      textPrimary: 'white',
+    },
+  },
+  typography: {
+    fontFamily: ['Arial'].join(','),
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    height: '100%',
+    width: 800,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
 export const App = () => {
+  const classes = useStyles();
   return (
     <AmplifyAuthenticator>
       <AmplifySignUp slot='sign-up' formFields={authFormFields} />
-      <div>
-        <ThemeProvider>
-          <CSSReset />
+      <div className={classes.root}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
           <HashRouter>
             <Switch>
               <Route exact path='/' component={AllPosts} />
